e56134f0e5af017b2924cf8fd80b4da9c31719be
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-21 14:50:04 -0400

    Completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,16 +3,14 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def new
-  end
+  def new; end
 
   def create
     @author = Author.create!(author_params)
-
     redirect_to author_path(@author)
   end
 
-  private
+private
 
   def author_params
     params.permit(:email, :name)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,20 @@
 class PostsController < ApplicationController
-  before_action :set_post!, only: [:show, :edit, :update]
+  before_action :set_post!, only: %i[show edit update]
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render("edit")
+    end
   end
 
-  private
+private
 
   def post_params
     params.permit(:category, :content, :title)
